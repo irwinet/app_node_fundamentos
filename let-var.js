5a86e98a5b677d2f96af5fa4d11b5ee1e3006a12
Irwin <irwinestradatorres@gmail.com>
2021-01-04 11:21:51 -0500

    Let And Var, Template String, Destructuracion

---
+++ let-var.js
@@ -0,0 +1,31 @@
+// var nombre = 'Wolverine';
+
+// if (true) {
+//     var nombre = 'Magneto';
+// }
+
+// var nombre = 'Wolverine1';
+// var nombre = 'Wolverine2';
+// var nombre = 'Wolverine3';
+// var nombre = 'Wolverine4';
+
+// let nombre = 'Wolverine';
+
+// if (true) {
+//     nombre = 'Magneto';
+// }
+
+// nombre = 'Wolverine1';
+// nombre = 'Wolverine2';
+// nombre = 'Wolverine3';
+// nombre = 'Wolverine4';
+
+// console.log(nombre);
+
+let i = 'Hola Mundo';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+
+console.log(i);
